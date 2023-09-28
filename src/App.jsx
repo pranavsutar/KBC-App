0521ba4870eb40c3c6282c2a9cc8ce0061f27782
Pranav <84005308+pranavsutar@users.noreply.github.com>
2023-09-28 16:17:56 +0530

    most of the initial background things done

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+
+const [queNum, setQueNum] = useState(1);
+const moneyPyramid = [
+    { id: 1, amount: "Rs. 5000" },
+    { id: 2, amount: "Rs. 8000" },
+    { id: 3, amount: "Rs. 10000" },
+    { id: 4, amount: "Rs. 15000" },
+    { id: 5, amount: "Rs. 25000" },
+    { id: 6, amount: "Rs. 50000" },
+    { id: 7, amount: "Rs. 100000" },
+    { id: 8, amount: "Rs. 200000" },
+    { id: 9, amount: "Rs. 400000" },
+    { id: 10, amount: "Rs. 800000" },
+    { id: 11, amount: "Rs. 1600000" },
+    { id: 12, amount: "Rs. 3200000" },
+    { id: 13, amount: "Rs. 6400000" },
+    { id: 14, amount: "Rs. 12500000" },
+    { id: 15, amount: "Rs. 25000000" }
+  ]
+  const incre = () => {
+    setQueNum(queNum + 1);
+  }
+  ;
+
+  return (
+    <div className="app">
+      <div className="main">
+        <button id="incre" onClick={incre}>
+          Incre</button>
+      </div>
+      <div className="pyramid">
+        <ul className="moneylist">
+            {moneyPyramid.map((m) => (
+
+              <li className={queNum !== m.id ? "moneylistitem": "moneylistitem active"}>
+                    <span className="moneylistitemnumber">{m.id}</span>
+                    <span className="moneylistitemamount">
+                      {m.amount}
+                    </span>
+              </li>
+            )).reverse()
+            }            
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
