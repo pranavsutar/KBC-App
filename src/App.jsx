e7b53a039a51db622c9e9a8cfdd021dda2d1dc12
Pranav <84005308+pranavsutar@users.noreply.github.com>
2023-09-29 20:05:29 +0530

    Most of the UI done

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
 import "./App.css";
+import Trivia from "./components/Trivia";
 import { useState } from "react";
 
 function App() {
 
 const [queNum, setQueNum] = useState(1);
 const moneyPyramid = [
-    { id: 1, amount: "Rs. 5000" },
-    { id: 2, amount: "Rs. 8000" },
-    { id: 3, amount: "Rs. 10000" },
-    { id: 4, amount: "Rs. 15000" },
-    { id: 5, amount: "Rs. 25000" },
-    { id: 6, amount: "Rs. 50000" },
-    { id: 7, amount: "Rs. 100000" },
-    { id: 8, amount: "Rs. 200000" },
-    { id: 9, amount: "Rs. 400000" },
-    { id: 10, amount: "Rs. 800000" },
-    { id: 11, amount: "Rs. 1600000" },
-    { id: 12, amount: "Rs. 3200000" },
-    { id: 13, amount: "Rs. 6400000" },
-    { id: 14, amount: "Rs. 12500000" },
-    { id: 15, amount: "Rs. 25000000" }
+    { id: "1", amount: "Rs. 5000" },
+    { id: "2", amount: "Rs. 8000" },
+    { id: "3", amount: "Rs. 10000" },
+    { id: "4", amount: "Rs. 15000" },
+    { id: "5", amount: "Rs. 25000" },
+    { id: "6", amount: "Rs. 50000" },
+    { id: "7", amount: "Rs. 100000" },
+    { id: "8", amount: "Rs. 200000" },
+    { id: "9", amount: "Rs. 400000" },
+    { id: "10", amount: "Rs. 800000" },
+    { id: "11", amount: "Rs. 1600000" },
+    { id: "12", amount: "Rs. 3200000" },
+    { id: "13", amount: "Rs. 6400000" },
+    { id: "14", amount: "Rs. 12500000" },
+    { id: "15", amount: "Rs. 25000000" }
   ]
   const incre = () => {
     setQueNum(queNum + 1);
@@ -29,14 +30,19 @@ const moneyPyramid = [
   return (
     <div className="app">
       <div className="main">
-        <button id="incre" onClick={incre}>
-          Incre</button>
+        {/* <button id="incre" onClick={incre}>Incre</button> */}
+        <div className="top">
+          <div className="timer">30 </div>
+        </div>
+        <div className="bottom">
+          <Trivia/>
+        </div>
       </div>
       <div className="pyramid">
         <ul className="moneylist">
             {moneyPyramid.map((m) => (
 
-              <li className={queNum !== m.id ? "moneylistitem": "moneylistitem active"}>
+              <li className={queNum != m.id ? "moneylistitem": "moneylistitem active"}>
                     <span className="moneylistitemnumber">{m.id}</span>
                     <span className="moneylistitemamount">
                       {m.amount}
---
+++ src/components/Trivia.jsx
@@ -0,0 +1,14 @@
+
+export default function Trivia() {
+    return (
+        <div className="trivia">
+            <div className="question">Ko dirgha rogo bhavati eva sadho? Kim aushadham tatra?</div>
+            <div className="answers">
+                <div className="answer">Bhava Bhava </div>
+                <div className="answer">Bhava Vichar</div>
+                <div className="answer">Vichar Bhava</div>
+                <div className="answer">Vichar Vichar</div>
+            </div>
+        </div>
+    );
+}
