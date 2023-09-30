f2fa5919aee3bb9918be99601224d8296c006401
Pranav <84005308+pranavsutar@users.noreply.github.com>
2023-09-30 16:20:18 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -12,34 +12,29 @@ function App() {
   const [stop, setStop] = useState(false);
   const [earned, setEarned] = useState("Rs. 24,00,000");
 
-const moneyPyramid = [
-    { id: "1", amount: "Rs. 5000" },
-    { id: "2", amount: "Rs. 8000" },
-    { id: "3", amount: "Rs. 10000" },
-    { id: "4", amount: "Rs. 15000" },
-    { id: "5", amount: "Rs. 25000" },
-    { id: "6", amount: "Rs. 50000" },
-    { id: "7", amount: "Rs. 100000" },
-    { id: "8", amount: "Rs. 200000" },
-    { id: "9", amount: "Rs. 400000" },
-    { id: "10", amount: "Rs. 800000" },
-    { id: "11", amount: "Rs. 1600000" },
-    { id: "12", amount: "Rs. 3200000" },
-    { id: "13", amount: "Rs. 6400000" },
-    { id: "14", amount: "Rs. 12500000"},
-    { id: "15", amount: "Rs. 25000000" }
-  ];
+const moneyPyramid = useMemo(()=> [
+    { id: 1, amount: "Rs. 5000" },
+    { id: 2, amount: "Rs. 8000" },
+    { id: 3, amount: "Rs. 10000" },
+    { id: 4, amount: "Rs. 15000" },
+    { id: 5, amount: "Rs. 25000" },
+    { id: 6, amount: "Rs. 50000" },
+    { id: 7, amount: "Rs. 100000" },
+    { id: 8, amount: "Rs. 200000" },
+    { id: 9, amount: "Rs. 400000" },
+    { id: 10, amount: "Rs. 800000" },
+    { id: 11, amount: "Rs. 1600000" },
+    { id: 12, amount: "Rs. 3200000" },
+    { id: 13, amount: "Rs. 6400000" },
+    { id: 14, amount: "Rs. 12500000"},
+    { id: 15, amount: "Rs. 25000000" }
+  ], []);
 
 
   useEffect(()=>{
-    queNum > 1 && setEarned(moneyPyramid.find((m)=> m.id == queNum-1)?.amount); 
-  }, [queNum])
+    queNum > 1 && setEarned(moneyPyramid.find((m)=> m.id === queNum-1)?.amount); 
+  }, [moneyPyramid, queNum])
 
-
-  const incre = () => {
-    setQueNum(queNum + 1);
-  }
-  ;
   if (userName === null) {
     return (
       <div className="app">
@@ -68,7 +63,7 @@ const moneyPyramid = [
           {stop ? <h1 className="endgame">
            <p> You earned: {earned}</p>
             
-            {queNum != 15 ? "Play gain to gain more money!" : "You have Completed the game!"}
+            {queNum !== 15 ? "Play gain to gain more money!" : "You have Completed the game!"}
             <p>Congradulations {userName} !!!</p>
             
             
@@ -99,7 +94,7 @@ const moneyPyramid = [
         <ul className="moneylist">
             {moneyPyramid.map((m) => (
 
-              <li className={queNum != m.id ? "moneylistitem": "moneylistitem active"}>
+              <li className={queNum !== m.id ? "moneylistitem": "moneylistitem active"}>
                     <span className="moneylistitemnumber">{m.id}</span>
                     <span className="moneylistitemamount">
                       {m.amount}
